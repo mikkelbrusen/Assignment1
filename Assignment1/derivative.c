24bdc3977d7a239d1d4ae5cff48c75dceaf982c2
Mikkel Mørch <mikkel.moerch2@live.dk>
2015-09-15 13:20:01 +0200

    start på derivative

--- Assignment1/derivative.c
+++ Assignment1/derivative.c
@@ -1,5 +1,43 @@
 #include "filters.h"
+#include "structures.h"
 
-int derivative(int x){
+struct DeStructure{
+	int h;
+	int x[5];
+	int y;
+};
 
+struct HPStructure Derivative(struct DeStructure a, int hpValue ){
+	struct HPStructure b;
+	b = a;
+	int x0;
+	int x1;
+	int x2;
+	int x3;
+	int x4;
+	int h = a.h;
+
+	a.x[h] = hpValue;
+	b.x[h] = hpValue;
+
+	x0 = a.x[h];
+	h = (h+1)%5;
+
+	x1 = a.x[h];
+	h = (h+1)%5;
+
+	x2 = a.x[h];
+	h = (h+1)%5;
+
+	x3 = a.x[h];
+	h = (h+1)%5;
+
+	x4 = a.x[h];
+
+	b.y = ;
+	b.h = h;
+
+	return b;
 }
+
+
