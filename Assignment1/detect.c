07c5a225755cbcf723ee0f29faf473582821f6ed
Mikkel Mørch <mikkel.moerch2@live.dk>
2015-09-16 15:46:13 +0200

    refactoring detect.c

--- Assignment1/detect.c
+++ Assignment1/detect.c
@@ -7,51 +7,59 @@ void gatherFT(int mwiValue, int index){
 }
 
 int peaks[30];
+int Rpeaks[30];
 
 int detect(int mwiValue ){
 	int spkf, npkf, threshold1, threshold2;
 	int rr_average1, rr_average2, rr_low, rr_high, rr_miss;
+	static int timer = 0;
+	static int index = 0;
+	static enum state{
+		CHECK_PEAK,CHECK_THRESHOLD,CALCULATE_RR,STORE_RPEAK
+	}
 
 	x[2] = mwiValue;
 
 	int state = 0;
-	switch(state){
-		case 0 :
-			if (x[0] < x[1] && x[1] > x[2]){
-			state = 1;
-			}
-			else {
-				state = 99;
-			}
-			break;
-		case 1 :
-			//Store in PEAKS
-			state = 2;
-			break;
-		case 2 :
-			//check with threshold
-			if(peak > threshold1){
-				npkf = 0.125*x[1] + 0.875*npkf;
-				threshold1 = npkf + 0.25*(spkf-npkf);
-				threshold2 = 0.5*threshold2;
 
-				state = 99;
-			}
-			else{
-				state = 3;
-			}
-			break;
-		case 3 :
-			//calculate RR
-			state = 4;
-			break;
-		case 4 :
-			if(RR_low<RR)
+	timer++;
 
-
-		case 99 :
-			return 0;
+	if (x[0] < x[1] && x[1] > x[2]){
+		peaks[index] = x[1];
+		timer = 0;
+		index++;
+		return checkThreshold(x[1]);
 	}
+	else {return 0;}
+}
+
+int checkThreshold(int x){
+
+}
+
+
+	break;
+case CHECK_THRESHOLD :
+	if(x[1] > threshold1){
+		state = STORE_RPEAK;
+	}
+	else{
+		npkf = 0.125*x[1] + 0.875*npkf;
+		threshold1 = npkf + 0.25*(spkf-npkf);
+		threshold2 = 0.5*threshold2;
+		state = 99;}
+	break;
+case STORE_RPEAK :
+	//calculate RR
+	//Vi skal tælle hvor mange iterationer der har været siden sidste peak
+	Rpeaks[index]=timer;
+
+	state = 4;
+	break;
+case 4 :
+
+case 99 :
+	return 0;
 }
 
 /*
