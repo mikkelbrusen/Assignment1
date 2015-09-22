ea0c5681b7e84fa7dee517c2436d4b3d03f9e550
Mikkel Mørch <mikkel.moerch2@live.dk>
2015-09-22 16:52:40 +0200

    Check_RR,Store RR,RR_recent,RR_recent_OK

--- Assignment1/detect.c
+++ Assignment1/detect.c
@@ -4,9 +4,13 @@ void checkThreshold(int);
 void storeRPeak(int);
 static int x[3] = {0};
 static int spkf, npkf, threshold1, threshold2;
-static int rr_average1, rr_average2, rr_low, rr_high, rr_miss;
+static int rr,rr_average1, rr_average2, rr_low, rr_high, rr_miss;
+//timer der tæller
+static int rPeak[500]= {0};
+static int recentRR_OK[500]= {0};
+static int recentRR[500] = {0};
 
-/*
+
 void gatherFT(int mwiValue, int index){
 	x[index] = mwiValue;
 }
@@ -14,6 +18,7 @@ void gatherFT(int mwiValue, int index){
 int detect(int mwiValue ){
 	x[2] = mwiValue;
 	if (x[0] < x[1] && x[1] > x[2]){
+		//Store timer value
 		checkThreshold(x[1]);
 		return 0; //should return something else than 0
 	}
@@ -29,13 +34,41 @@ void checkThreshold(int x){
 		threshold2 = 0.5*threshold2;
 	}
 }
-
-void storeRPeak(int x){
-	//calculate RR
-	//Vi skal tælle hvor mange iterationer der har været siden sidste peak
+void checkForRR(int x,int y){
+	//Beregner RR (tiden fra sidste peak)
+	rr = x-y;
+	//Tjekker om det er en OK RR-interval
+	if(rr_low<rr<rr_high){
+		storeRPeak(rr);
+		storeRecentRR(rr);
+		storeRecentOK(rr);
+		commonUpdate(rr);
+		calcRRAve(recentRR_OK);
+		calcRRAVE(recentRR);
+		commonUpdate()
+	}
 
 }
-*/
+void storeRR(int x){
+	//Gemmer Rpeak rPeak
+	static int hRR = 0;
+	rr[hRR]=x;
+	hRR = (hRR + 1) % 500;
+}
+void storeRecentRR(int x){
+	static int hRecent;
+	recentRR[hRecent]=x;
+	hRecent = (hRecent + 1) % 500;
+}
+void storeRecentOK(int x){
+	static int hRecentOK;
+	recentRR_OK[hRecentOK] = x;
+	hRecentOK = (hRecentOK+1) % 500;
+}
+
+
+
+
 
 
 
