25bd33d53427335d8a87931b69749c4b2326eb7d
Mikkel Møller Brusen <Mikkel_mb@hotmail.com>
2015-09-22 17:29:34 +0200

    Fix: Merge

--- Assignment1/detect.c
+++ Assignment1/detect.c
@@ -9,34 +9,39 @@ static int rr,rr_average1, rr_average2, rr_low, rr_high, rr_miss;
 static int rPeak[500]= {0};
 static int recentRR_OK[500]= {0};
 static int recentRR[500] = {0};
-
-
+static int peaks[500] = {0};
+static int hpeaks = 0;
 
 //Reads in the first two data points
 void gatherFT(int mwiValue, int index){
 	x[index] = mwiValue;
 }
 
-int detect(int mwiValue ){
+void detect(int mwiValue ){
+	//TODO: Increment timer
 	x[2] = mwiValue;
 	if (x[0] < x[1] && x[1] > x[2]){
-		//Store timer value
+		//TODO: Store time in peak timer array
 		storePeak(x[1]);
 		checkThreshold(x[1]);
-		return 0; //should return something else than 0
 	}
-	else {return 0;}
+}
+
+void storePeak(int x){
+	peaks[hpeaks] = x;
+	hpeaks = (hpeaks+1) % 500;
 }
 
 void checkThreshold(int x){
-	if(x[1] > threshold1){
-			storeRPeak(x);
+	if(x > threshold1){
+		checkForRR(x);
 	} else{
-		npkf = 0.125*x[1] + 0.875*npkf;
+		npkf = 0.125*x + 0.875*npkf;
 		threshold1 = npkf + 0.25*(spkf-npkf);
 		threshold2 = 0.5*threshold2;
 	}
 }
+
 void checkForRR(int x,int y){
 	//Beregner RR (tiden fra sidste peak)
 	rr = x-y;
@@ -45,34 +50,65 @@ void checkForRR(int x,int y){
 		storeRPeak(rr);
 		storeRecentRR(rr);
 		storeRecentOK(rr);
-		commonUpdate(rr);
-		calcRRAve(recentRR_OK);
-		calcRRAVE(recentRR);
-		commonUpdate()
+		rr_average2 = calcRRAve(recentRR_OK);
+		rr_average1 = calcRRAVE(recentRR);
+		commonUpdate(rr_average2);
 	}
+}
 
+void storeRPeak(int x){
+	static int hRPeak = 0;
+	rPeak[hRPeak]=x;
+	hRPeak = (hRPeak + 1) % 500;
 }
-void storeRR(int x){
-	//Gemmer Rpeak rPeak
-	static int hRR = 0;
-	rr[hRR]=x;
-	hRR = (hRR + 1) % 500;
-}
+
 void storeRecentRR(int x){
-	static int hRecent;
+	static int hRecent = 0;
 	recentRR[hRecent]=x;
 	hRecent = (hRecent + 1) % 500;
 }
 void storeRecentOK(int x){
-	static int hRecentOK;
+	static int hRecentOK = 0;
 	recentRR_OK[hRecentOK] = x;
 	hRecentOK = (hRecentOK+1) % 500;
 }
 
+void peak2Update(int peak){
+	storeRPeak(peak);
+	spkf = 0.25*peak + 0.75*spkf;
+	storeRecentRR(rr);
+	rr_average1 = calcRRAve(recentRR);
+	commonUpdate(rr_average1);
+}
 
+void commonUpdate(int rr_av){
+	rr_low = 0.92*rr_av;
+	rr_high = 1.16*rr_av;
+	rr_miss = 1.66*rr_av;
+	threshold1 = npkf + 0.25*(spkf-npkf);
+	threshold2 = 0.5*threshold1;
+}
 
+int calcRRAve(int x[]){
+	int length = sizeof(x)/sizeof(x[0]);
+	int average = 0;
+	for(int i = 0; i<length; i++){
+		average += x[i];
+	}
+	return average;
+}
 
-
+void searchBack(){
+	int hsB = hpeaks;
+	for (int i=0; i<500; i++) {
+		if(peaks[hsB] > threshold2) {
+			storeRPeak(peaks[hsB]);
+			peak2Update();
+			break;
+		} else {
+			hsB = (hsB + 499) % 500;
+		}
+	}
 }
 
 
