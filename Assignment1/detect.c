7ff41d5fccd24a6136dc42f6606e7dbb9c8c7b12
Mikkel Møller Brusen <Mikkel_mb@hotmail.com>
2015-09-22 15:51:17 +0200

    Add: CheckThreshhold and StorePeak

--- Assignment1/detect.c
+++ Assignment1/detect.c
@@ -1,30 +1,36 @@
 #include "filters.h"
 
 void checkThreshold(int);
-void storeRPeak(int);
+void storePeak(int);
 static int x[3] = {0};
 static int spkf, npkf, threshold1, threshold2;
 static int rr_average1, rr_average2, rr_low, rr_high, rr_miss;
+static int peaks[500] = {0};
+static int hpeaks = 0;
 
-/*
+//Reads in the first two data points
 void gatherFT(int mwiValue, int index){
 	x[index] = mwiValue;
 }
 
-int detect(int mwiValue ){
+void detect(int mwiValue ){
 	x[2] = mwiValue;
 	if (x[0] < x[1] && x[1] > x[2]){
+		storePeak(x[1]);
 		checkThreshold(x[1]);
-		return 0; //should return something else than 0
 	}
-	else {return 0;}
+}
+
+void storePeak(int x){
+	peaks[hpeaks] = x;
+	hpeaks = (hpeaks+1) % 500;
 }
 
 void checkThreshold(int x){
-	if(x[1] > threshold1){
-			storeRPeak(x);
+	if(x > threshold1){
+			checkForRR(x);
 	} else{
-		npkf = 0.125*x[1] + 0.875*npkf;
+		npkf = 0.125*x + 0.875*npkf;
 		threshold1 = npkf + 0.25*(spkf-npkf);
 		threshold2 = 0.5*threshold2;
 	}
