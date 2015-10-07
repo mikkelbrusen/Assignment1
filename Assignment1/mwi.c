de6652b7dfeba40370031606027a90c9d686cca9
Mikkel Møller Brusen <Mikkel_mb@hotmail.com>
2015-10-07 14:35:36 +0200

    Add: Much faster mwi filter

--- Assignment1/mwi.c
+++ Assignment1/mwi.c
@@ -3,14 +3,12 @@
 int movingWI( int sqValue ){
 	static int h = 0;
 	static int x[30] = {0};
+	static int x_sum = 0;
 	int y;
-	int x_sum = 0;
 
+	x_sum -=x[h];
+	x_sum += sqValue;
 	x[h] = sqValue;
-
-	for(int i = 0;i<30;i++){
-		 x_sum += x[i];
-	}
 
 	h = (h+1)%30;
 	y = x_sum/30;
